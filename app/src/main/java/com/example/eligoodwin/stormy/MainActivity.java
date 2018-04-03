a95ffbe4945d36ec11007af044a7338d86b8c23a
eligoodwin <goodwiel@oregonstate.edu>
2018-04-02 20:04:44 -0700

    messing around

--- app/src/main/java/com/example/eligoodwin/stormy/MainActivity.java
+++ app/src/main/java/com/example/eligoodwin/stormy/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.eligoodwin.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -20,26 +26,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String apiKey = "3135cc7c4e2d2c1f1e652b8695dcf7bc";
-        double latitude = 37.8267;
+//        double latitude = 37.8267;
+        double latitude = 5555;
         double longitude = -122.4233;
         String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(forecastUrl)
-                .build();
+        if(isNetworkIsAvaialable()) {
 
-        Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if(response.isSuccessful()){
-                Log.v(TAG, response.body().string());
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: " , e);
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
 
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+
+                    }
+                }
+            });
         }
+        else{
+            Toast.makeText(this, R.string.connection_status_toast, Toast.LENGTH_SHORT).show();
+        }
+
     }
 
+    private boolean isNetworkIsAvaialable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
+    }
 
 }
